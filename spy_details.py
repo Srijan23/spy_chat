1c83875aefaa68753e43cc1aacf40bce7c09d165
Srijan23 <sgsrijangupta2468@gmail.com>
2017-09-18 12:11:23 +0530

    project

---
+++ spy_details.py
@@ -0,0 +1,25 @@
+
+
+class Spy:
+
+    def __init__(self, name, salutation, age, rating):
+
+        # initializing the values
+
+        self.name = name
+
+        self.salutation = salutation
+
+        self.age = age
+
+        self.rating = rating
+
+        self.is_online = True
+
+        self.chats = []
+
+        self.name = self.salutation + ' ' + self .name
+
+        self.spy_stat = None
+
+    spy = Spy ( ' shashwat ' , 'Mr' , 21 , 4.8 )
